9a3caa9a0235467b83c2b7ae4d82f7975549cd1e
verissteph <stephanieverissimopp@gmail.com>
2020-04-07 23:25:55 -0300

    criação conteudo de tecnologia

--- js/noticias.js
+++ js/noticias.js
@@ -65,3 +65,56 @@ async function pegarNoticias() {
 
     console.log(listaDeNoticias);
 }
+
+
+let containerDeNoticiasTecnologia = document.querySelector('#listaDeNoticiasTecnologia')
+async function pegarNoticiasTecnologia() {
+
+    let noticiasTecnologia = await fetch('http://newsapi.org/v2/top-headlines?country=br&category=technology&apiKey=82e74070f02b4a2498324d600607c30f');
+    let listaDeNoticiasTecnologia = await noticiasTecnologia.json();
+
+    listaDeNoticiasTecnologia.articles.forEach((dados) => {
+        let coluna = document.createElement('div');
+        coluna.setAttribute('class', 'col-3');
+
+        let card = document.createElement('div');
+        card.setAttribute('class', 'card');
+
+        let imgCard = document.createElement('img')
+        imgCard.setAttribute('class', 'card-img-top')
+        imgCard.setAttribute('src', dados.urlToImage);
+
+        let bodyCard = document.createElement('div')
+        bodyCard.setAttribute('class', 'card-body');
+
+        let titleCard = document.createElement('h5')
+        titleCard.setAttribute('class', 'card-title');
+        titleCard.textContent = dados.title;
+
+        let descriptionCard = document.createElement('p')
+        descriptionCard.setAttribute('class', 'card-text');
+        descriptionCard.textContent = dados.description;
+
+        let linkCard = document.createElement('a')
+        linkCard.setAttribute('class', 'btn btn-primary');
+        linkCard.setAttribute('href', dados.url);
+        linkCard.textContent = "Ir para noticia";
+
+        card.appendChild(imgCard);
+        card.appendChild(bodyCard);
+        bodyCard.appendChild(titleCard);
+        bodyCard.appendChild(descriptionCard);
+        bodyCard.appendChild(linkCard);
+
+        coluna.appendChild(card);
+
+        containerDeNoticiasTecnologia.appendChild(coluna)
+
+
+
+    })
+
+
+
+    console.log(listaDeNoticiasTecnologia);
+}
